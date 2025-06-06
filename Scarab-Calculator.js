df3329067cb5e926bf9633e3e1def270559dcacb
Neutronlul <53470063+Neutronlul@users.noreply.github.com>
2025-06-06 19:26:39 -0400

    Update scarab-calculator.js

--- Scarab-Calculator.js
+++ Scarab-Calculator.js
@@ -1,4 +1,29 @@
+const ROUTE_PATTERNS = {
+  exclude: [
+    /snapshotID=/
+  ],
+
+  include: [
+    /^\/$/,
+    /^\/stash/
+  ]
+};
+
+function validPath() {
+    const { pathname, search } = window.location;
+
+    if (ROUTE_PATTERNS.exclude.some(p => p.test(search))) {
+        return false;
+    }
+    return ROUTE_PATTERNS.include.some(p => p.test(pathname));
+}
+
 /*
-Just draw a border round the document.body.
+function tableInjection() {
+
+}
+
+const table = document.querySelector("table.mantine-Table-table");
 */
+
 document.body.style.border = "5px solid red";
